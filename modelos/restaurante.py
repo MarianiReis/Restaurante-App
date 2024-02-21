1ed6d109d66d13358806ad8a66bf5f5258debdcb
MarianiReis <contatomarianireis@gmail.com>
2024-02-20 22:13:51 -0300

    primeiro commit

---
+++ modelos/restaurante.py
@@ -0,0 +1,42 @@
+from modelos.avaliacao import Avaliacao
+
+class Restaurante: #classe restaurante
+    restaurantes = [] #lista de restaurantes
+    def __init__(self, nome, categoria): #construtor 
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacoes = []
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self): #metodo que imprime o construtor
+        return f"{self._nome} | {self._categoria}"
+    
+    @classmethod
+    def listar_restaurante(cls): #metodo que imprime todos os restaurantes da lista
+        print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} | {"Status"}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacao).ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '☒' if self._ativo else '☐'
+        
+    def alternando_estado_do_restaurante(self):
+        self._ativo = not self._ativo
+    
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota < 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacoes.append(avaliacao)
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacoes:
+            return f'Restaurante sem avaliação'
+        
+
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacoes)
+        quantidade_de_notas = len(self._avaliacoes)
+        media_restaurante = round(soma_das_notas / quantidade_de_notas ,1)
+        return media_restaurante
