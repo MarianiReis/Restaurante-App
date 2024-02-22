9ce3e5bb71b0880377b5402734f6475b33bed424
Mariani Reis dos Santos <109634197+MarianiReis@users.noreply.github.com>
2024-02-22 00:11:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ from modelos.restaurante import Restaurante
 restaurante_los_chicos = Restaurante('los chicos', 'mexicana')
 restaurante_los_chicos.alternando_estado_do_restaurante()
 
-restaurante_los_chicos.receber_avaliacao('Guilherme', 10)
-restaurante_los_chicos.receber_avaliacao('Maria', 8)
-restaurante_los_chicos.receber_avaliacao('Luis', 9)
-
-
-
-
 
 def main():
     Restaurante.listar_restaurante()
